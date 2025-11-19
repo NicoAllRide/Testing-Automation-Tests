86c336de9670025d3b92664fe96c84225b83ef0d
NicoAllRide <167778137+NicoAllRide@users.noreply.github.com>
2025-11-19 17:38:55 -0300

    Update WebSocketClient.py

--- AllRide Test Cases/Test Cases/Carpool/WebSocketClient.py
+++ AllRide Test Cases/Test Cases/Carpool/WebSocketClient.py
@@ -1,10 +1,33 @@
-from .keywords import WebSocketClientKeywords
-from .version import get_version
-
-__version__ = get_version()
+from websocket import create_connection
 
 
-class WebSocketClient(WebSocketClientKeywords):
+class WebSocketClient:
+    """Keywords para manejar WebSocket en Robot Framework."""
 
-    ROBOT_LIBRARY_VERSION = __version__
-    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    def connect(self, url, timeout=None, **options):
+        # Mantén vivo el socket con ping automático
+        return create_connection(
+            url,
+            timeout=timeout,
+            ping_interval=10,
+            ping_timeout=5,
+            **options,
+        )
+
+    def send(self, websocket, message):
+        return websocket.send(message)
+
+    def recv(self, websocket):
+        return websocket.recv()
+
+    def recv_data(self, websocket, control_frame=False):
+        return websocket.recv_data(control_frame)
+
+    def ping(self, websocket, payload=""):
+        websocket.ping(payload)
+
+    def pong(self, websocket, payload=""):
+        websocket.pong(payload)
+
+    def close(self, websocket):
+        websocket.close()
